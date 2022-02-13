68531b1bb8366f4a267376286659cc6070f73e02
Priya Sharma <77352100+DevPriSha@users.noreply.github.com>
2022-02-13 23:39:16 +0530

    add verify command

--- commands/verify.js
+++ commands/verify.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-
+const { branch_list, years } = require('../config.json');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('verify')
@@ -9,7 +9,28 @@ module.exports = {
 				.setDescription('Your 11 digit IGDTUW roll no')
 				.setRequired(true)),
 	async execute(interaction) {
-		//const rollnumber = interaction.getOption('rollnumber');
-		await interaction.reply({content: 'Ask Pri to tell me how does this whole verifying thing works, then I\'ll verify you ', ephemeral: true});
+		const rollnumber = interaction.options.getString('rollnumber');
+		//match rollnumber with regex pattern
+		while (rollnumber.match(/^[0-9]{3}[0-9]{4}20[0-9]{2}$/)) {
+			const roll = rollnumber.substring(0, 3);
+			const branch = rollnumber.substring(3, 7);
+			const year = rollnumber.substring(7, 11);
+			
+			//check if branch is in branch_list
+			if (!branch_list[branch]) {
+				console.log(`${rollnumber} is not a valid roll number.`);
+				break;
+			}
+			//give the user the correct role
+			if(branch === '0101' || branch === '0102' || branch === '0103' || branch === '0104' || branch === '1016' || branch === '0117' || branch === '0118')
+				await interaction.member.roles.add('921492490157162587');
+			
+			await interaction.member.roles.add(years[year]);
+			await interaction.member.roles.add(branch_list[branch]);
+			await interaction.reply({content: `Successfully verified! Welcome to IGDTUW! \n  Your roll number: ${roll} \n Your Branch: <@&${branch_list[branch]}> \n Your Year of joining: ${year}`, ephemeral: true});
+
+			return;
+		}
+		await interaction.reply({content: 'Invalid roll number. Are you sure you are a student of IGDTUW? If yes, then contact <@&921485871750987848>s', ephemeral: true});
 	},
 };
